9c129f753513a68c7a8440068e6072b74af7693e
mrs-y-arai <sidvivid9672@gmail.com>
2024-08-31 00:57:46 +0900

    prefetchをやめる

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,7 +4,7 @@ import { HamburgerMenu } from '~/components/Menu/HamburgerMenu';
 export function Header() {
   return (
     <header className="flex h-[70px] items-center justify-center border-b py-2">
-      <Link className="text-xl font-bold" href="/" prefetch={true}>
+      <Link className="text-xl font-bold" href="/">
         みんなのエンジニアクイズ
       </Link>
       <HamburgerMenu className="absolute right-4 top-4" />
