e8652ceb31b13d097cfe1122b67e021cd5f19946
mrs-y-arai <sidvivid9672@gmail.com>
2024-09-14 02:06:33 +0900

    testの準備

---
+++ src/utils/index.spec.ts
@@ -0,0 +1,25 @@
+import { expect, it, describe } from 'vitest';
+import { generateXShareUrl, getApiBaseUrl } from './index';
+describe('generateXShareUrl', () => {
+  it('should return the correct URL', () => {
+    // Arrange
+    const text = 'test';
+    const path = '/test';
+
+    // Act
+    const result = generateXShareUrl({ text, path });
+
+    // Assert
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    expect(result).toBe(
+      `https://twitter.com/intent/tweet?text=${text}&url=${baseUrl}${path}&hashtags=エンジニアクイズ`,
+    );
+  });
+});
+
+describe('getApiBaseUrl', () => {
+  it('should return true', () => {
+    const result = getApiBaseUrl();
+    expect(result).toBe(true);
+  });
+});
