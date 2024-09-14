e653be539a90604ada252386ac3a15d16b0e4969
mrs-y-arai <sidvivid9672@gmail.com>
2024-09-14 12:11:51 +0900

    style調整

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'placeholder:text-muted-foreground flex min-h-[80px] w-full grow rounded border border-gray-light bg-background px-3 py-2 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'placeholder:text-muted-foreground flex min-h-[80px] w-full grow rounded border border-gray-light bg-white px-3 py-2 text-sm text-black ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
