f1d7689cbe44069f9cc8d86dcb79d7d1119de110
mrs-y-arai <sidvivid9672@gmail.com>
2024-09-14 11:52:46 +0900

    lint:fix・非公開画面設定

--- src/components/Loading/LoadingUI.tsx
+++ src/components/Loading/LoadingUI.tsx
@@ -2,7 +2,7 @@ export function LoadingUI() {
   return (
     <>
       <div
-        className="bg-light-gray/70 z-loading fixed left-0 top-0 flex h-screen w-screen items-center justify-center"
+        className="bg-light-gray/70 fixed left-0 top-0 z-loading flex h-screen w-screen items-center justify-center"
         aria-label="読み込み中"
       >
         <div className="size-10 animate-spin rounded-full border-4 border-primary border-t-transparent" />
